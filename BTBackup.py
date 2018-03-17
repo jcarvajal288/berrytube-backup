f2c1e710fc7be7f4719a4d258c1e47508c6bc52d
Max Carvajal <fake@email.net>
2018-03-17 14:53:36 -0500

    Adds multi-file downloading and a custom logger to catch download errors for later processing.

--- BTBackup.py
+++ BTBackup.py
@@ -25,6 +25,38 @@ class Video(object):
     def incrementCount(self):
         self.playCount += 1
 
+
+class Logger(object):
+    def __init__(self, ydl):
+        self.errors = []
+        self.ydl = ydl
+
+    def to_stdout(self, message, skip_eol=False, check_quiet=False):
+        """Print message to stdout if not in quiet mode."""
+        if not check_quiet or not self.ydl.params.get('quiet', False):
+            message = self.ydl._bidi_workaround(message)
+            terminator = ['\n', ''][skip_eol]
+            output = message + terminator
+            self.ydl._write_string(output, self.ydl._screen_file)
+
+    def to_stderr(self, message):
+        """Print message to stderr."""
+        message = self.ydl._bidi_workaround(message)
+        output = message + '\n'
+        self.ydl._write_string(output, self.ydl._err_file)
+
+    def debug(self, msg):
+        skip_eol = ' ETA ' in msg
+        self.to_stdout(msg, skip_eol)
+
+    def warning(self, msg):
+        self.to_stdout(msg)
+
+    def error(self, msg):
+        self.errors.append(msg)
+        self.to_stderr(msg)
+
+
 def parseArgs():
     parser = argparse.ArgumentParser(description=__doc__)
     parser.add_argument('-t', '--target', metavar='<directory>', type=str, dest='targetDirectory', required=False,
@@ -57,23 +89,30 @@ def readVidLog():
     return videosById
 
 
+def filterVideos(videosById, requiredPlays):
+    print("Filtering out videos with fewer than {} plays.".format(requiredPlays))
+    return [v for v in videosById.values() if v.playCount >= requiredPlays and v.source == 'yt']
+
+
 def performDownload(videosToDownload, targetDirectory):
     pathlib.Path(targetDirectory).mkdir(parents=True, exist_ok=True)
     video = videosToDownload[10]
-    videoUrl = "https://www.youtube.com/watch?v={}".format(video.vidId)
+    urls = ["https://www.youtube.com/watch?v={}".format(v.vidId) for v in videosToDownload]
     options =  {
-        'output': "%(title)s-%(id)s.%(ext)s"    
+        'ignoreerrors': True,
+        'output': "%(title)s-%(id)s.%(ext)s"
     }
     previousWorkingDirectory = os.getcwd()
     os.chdir(targetDirectory)
     with youtube_dl.YoutubeDL(options) as ydl:
-        ydl.download([videoUrl])
+        ydl.params['logger'] = Logger(ydl) # done here to reuse the default logger's nifty screen logging
+        ydl.download(urls[:10])
     os.chdir(previousWorkingDirectory)
 
 
 def main():
     targetDirectory = "V:/Media/berrytubeBackup/"
-    requiredPlays = 2
+    requiredPlays = 5
 
     args = parseArgs()
     if args.targetDirectory is not None:
@@ -86,7 +125,7 @@ def main():
 
     videosById = readVidLog()
     print("Found {} unique videos.".format(len(videosById)))
-    videosToDownload = [v for v in videosById.values() if v.playCount >= requiredPlays and v.source == 'yt']
+    videosToDownload = filterVideos(videosById, requiredPlays)
     print("Will download {} videos to {}".format(len(videosToDownload), targetDirectory))
     answer = input("Do you want to continue? (yes/no)")
     if answer == 'y' or answer == 'yes':
