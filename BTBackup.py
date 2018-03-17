aff6e0b361b1c5c0b4cf532abad0a6a253815ebd
Max Carvajal <fake@email.net>
2018-03-17 10:18:21 -0500

    Changes Video tuple into Video class.  Counts video plays.

--- BTBackup.py
+++ BTBackup.py
@@ -10,27 +10,50 @@ import urllib.request
 
 Video = collections.namedtuple('Video', 'timestamp source vidId title')
 
+class Video(object):
+    def __init__(self, line):
+        data = line.decode('utf-8').strip().split('<>')
+        self.timestamp = data[0]
+        self.source = data[1]
+        self.vidId = data[2]
+        self.title = data[3]
+        self.playCount = 1
+
+    def incrementCount(self):
+        self.playCount += 1
+
 def parseArgs():
     parser = argparse.ArgumentParser(description=__doc__)
     parser.add_argument('-t', '--target', metavar='<directory>', type=str, dest='targetDirectory', required=False,
             help='directory to put the downloaded videos.  Will be created if it does not exist.')
     parser.add_argument('-r', '--requiredPlays', metavar='<integer>', type=int, dest='requiredPlays', required=False,
             help='number of plays a single video needs to have in vidlog.txt to warrant backing up.')
+    #parser.add_argument('-v', '--verbose', help='verbose output')
     return parser.parse_args()
 
 
-def readVidLog():
+def readVidLog(requiredPlays):
     vidLogUrl = 'http://radio.berrytube.tv/vidlog.txt'
     vidlog = urllib.request.urlopen(vidLogUrl)
-    videos = collections.defaultdict(int)
+    videosById = {}
+    errors = 0
     for line in vidlog:
         try:
-            video = Video(*line.decode('utf-8').strip().split('<>'))
+            video = Video(line)
         except TypeError:
             # TODO: log errored videos
+            errors += 1
             continue
-        videos[video] += 1
-    print(videos.popitem())
+        if video.vidId in videosById:
+            videosById[video.vidId].incrementCount()
+        else:
+            videosById[video.vidId] = video
+    videosToDownload = [v for v in videosById.values() if v.playCount >= requiredPlays]
+    if errors > 0:
+        print("Unable to parse {} lines of vidlog.txt".format(errors))
+    print("Will download {}/{} videos.".format(len(videosToDownload), len(videosById)))
+    return videosToDownload
+
 
 
 
@@ -44,7 +67,7 @@ def main():
     if args.requiredPlays is not None:
         requiredPlays = args.requiredPlays
 
-    videos = readVidLog()
+    videosToDownload = readVidLog(requiredPlays)
 
 
 if __name__ == "__main__":
